123ee041cef826a3c34a70afed304979bb480a68
Geetansh Goyal <geetanshg2@gmail.com>
2026-02-28 18:42:24 +0530

    feat: added to custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,22 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { createJiti } from 'jiti'
+import { fileURLToPath } from 'url'
+
+const jiti = createJiti(fileURLToPath(import.meta.url))
+const { customPlugin, getBaseRules } = await jiti.import('./src/Eslint/eslint.ts')
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      import: importPlugin,
+      custom: customPlugin,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,6 +28,19 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.app.json',
+        },
+      },
+    },
+    rules: {
+      ...getBaseRules(),
     },
   },
 ])
